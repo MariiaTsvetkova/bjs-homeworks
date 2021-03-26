cfdddc1c0ee2e977a821e2667c5a2c39a0af846b
Maria Tsvetkova <maria-french@mail.ru>
2021-03-26 11:24:23 +0300

    First commit data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,55 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (isNaN(validateNumber(percent))) {
+
+        return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+    }
+
+    if (isNaN(validateNumber(contribution))) {
+
+        return `Параметр "Сумма первоначального взноса" содержит неправильное значение ${contribution}`;
+
+    }
+
+    if (isNaN(validateNumber(amount))) {
+
+        return `Параметр "Сумма кредита" содержит неправильное значение ${amount}`;
+    } 
+
+    let currentYear = new Date().getFullYear();
+    let currentMonth = new Date().getMonth();
+    let yearOfPayment = date.getFullYear();
+    let monthOfPayment = date.getMonth();
+    let numberOfYears = yearOfPayment - currentYear;
+    let numberOfMonths = numberOfYears * 12 + (monthOfPayment - currentMonth);
+
+    let loanBody = amount - contribution;
+    let p = percent / 100 / 12;
+    
+    let monthlyPayment = loanBody * (p + p / (((1 + p) ** numberOfMonths) - 1));
+    let totalAmount = (monthlyPayment * numberOfMonths).toFixed(2);
+    
+    
+    return totalAmount;
+}
+
+function validateNumber(number) {
+    if (typeof number === "number") {
+        return number;
+    } 
+    
+    return +number;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    
+    if (!name) {
+        name = "Аноним";
+    }
+
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+    console.log(greeting);
+    
+    return greeting;
 }
